4b33ebf8bd995078f67d725738b1f4ad12634edc
Ivette <car16020@uvg.edu.gt>
2018-10-20 00:26:57 -0600

    models

--- chismes/chismes/settings.py
+++ chismes/chismes/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'mischismes',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'chismes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'chismes',
+        'USER': 'postgres',
+        'PASSWORD': 'Cardona98',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
