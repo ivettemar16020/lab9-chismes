d1ff94ac8453ce339e9212492d3f550a163dea05
Ivette <car16020@uvg.edu.gt>
2018-10-20 00:33:03 -0600

    test

--- chismes/mischismes/tests.py
+++ chismes/mischismes/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Chisme
+
+
+class ChismeModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        Chisme.objects.create(title='first chisme')
+        Chisme.objects.create(content='content here')
+
+    def test_title_content(self):
+        chisme = Chisme.objects.get(id=1)
+        expected_object_name = f'{chisme.title}'
+        self.assertEquals(expected_object_name, 'first chisme')
+
+    def test_content_content(self):
+        chisme = Chisme.objects.get(id=2)
+        expected_object_name = f'{chisme.content}'
+        self.assertEquals(expected_object_name, 'content here')
